584182d23b14ebb8f0c5db37fa7086c07ff3e0f1
Dominik Meduna <medunadominik@gmail.com>
2023-06-09 14:41:28 +0200

    Revert "-consulatation"
    This reverts commit 93be6521b58019e793694dc33dcbeabfd7fa6528.

--- backend/models/client.js
+++ backend/models/client.js
@@ -1,37 +1,31 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const clientSchema = new mongoose.Schema({
-  first_name: {
-    type: String,
-    require: true,
-  },
+    first_name: {
+        type: String,
+        require: true
+    },
 
-  last_name: {
-    type: String,
-    require: true,
-  },
+    last_name: {
+        type: String,
+        require: true
+    },
 
-  age: {
-    type: Number,
-    require: true,
-  },
-  phone_number: {
-    type: Number,
-    require: true,
-  },
-  street: {
-    type: String,
-    require: true,
-  },
-  city: {
-    type: String,
-    require: true,
-  },
+    age: {
+        type: Number,
+        require: true
+    },
+    phone_number: {
+        type: Number,
+        require: true
+    },
 
-  insurance: {
-    type: Array,
-    require: false,
-  },
-});
+    insurance:{
+        type: Array,
+        require: false
+    },
 
-module.exports = mongoose.model("Clients", clientSchema);
+
+})
+
+module.exports = mongoose.model('Clients', clientSchema)
